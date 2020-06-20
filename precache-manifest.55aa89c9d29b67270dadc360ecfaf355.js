b120ccbd03403a1e5f7e3cb94a1bec58ac08623d
AlexZhaba <sasazhava13@mail.ru>
2020-06-20 13:25:58 +0300

    Updates

---
+++ precache-manifest.55aa89c9d29b67270dadc360ecfaf355.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b0146afed1e0d0ace7c22f82f9e881",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3d30bb105d82e0fc3e14",
+    "url": "./static/css/main.a0c33a2e.chunk.css"
+  },
+  {
+    "revision": "e1cfe38a43aded109985",
+    "url": "./static/js/2.225556fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.225556fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d30bb105d82e0fc3e14",
+    "url": "./static/js/main.8173e6de.chunk.js"
+  },
+  {
+    "revision": "4b3a0bf093f9847b4d1d",
+    "url": "./static/js/runtime-main.b5b29441.js"
+  },
+  {
+    "revision": "6206770a032d43528570a026be9e3526",
+    "url": "./static/media/fon.6206770a.png"
+  },
+  {
+    "revision": "959a77a31fbb0c1b5ff5a911d3ae60b1",
+    "url": "./static/media/fon.959a77a3.png"
+  },
+  {
+    "revision": "ccb904b942e6ee06258a980655eb7655",
+    "url": "./static/media/sign_1.ccb904b9.jfif"
+  }
+]);
